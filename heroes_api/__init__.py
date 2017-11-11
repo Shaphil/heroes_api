8fab6a258a8b6f3de7ccf87f09b75c980485d447
Shaphil <shaphil2025@gmail.com>
2017-11-11 22:49:35 +0600

    Add CORS to app, HeroApi to resources.heroes_list

--- heroes_api/__init__.py
+++ heroes_api/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object('heroes_api.settings.DevelopmentConfig')
+CORS(app)
 api = Api(app)
 db = SQLAlchemy(app)
 
-from heroes_api.resources.heroes_list import HeroesListApi
+from heroes_api.resources.heroes_list import HeroApi, HeroesListApi
 
+api.add_resource(HeroApi, '/api/heroes/<id>', endpoint='hero_api')
 api.add_resource(HeroesListApi, '/api/heroes')
--- heroes_api/resources/heroes_list.py
+++ heroes_api/resources/heroes_list.py
@@ -4,8 +4,16 @@ from heroes_api.models.hero import Hero
 
 hero_fields = {
     'id': fields.Integer,
-    'name': fields.String
+    'name': fields.String,
+    'url': fields.Url(endpoint='hero_api')
 }
+
+
+class HeroApi(Resource):
+    @marshal_with(hero_fields)
+    def get(self, id):
+        hero = Hero.query.filter_by(id=id).first()
+        return hero
 
 
 class HeroesListApi(Resource):
