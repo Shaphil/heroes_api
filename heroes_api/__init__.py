ce25e8a7f803ecc4bdc00bfe2aa56712ccfd33a5
Shaphil <shaphil2025@gmail.com>
2017-11-11 22:31:06 +0600

    Load config, instantiate SQLAlchemy

--- heroes_api/__init__.py
+++ heroes_api/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_restful import Api
-
-from heroes_api.resources.hero import Hero
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object('heroes_api.settings.DevelopmentConfig')
 api = Api(app)
+db = SQLAlchemy(app)
 
-api.add_resource(Hero, '/api/heroes')
+from heroes_api.resources.heroes_list import HeroesListApi
+
+api.add_resource(HeroesListApi, '/api/heroes')
